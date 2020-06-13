6e8f4f4300886169db6929507a7cd573827e8f49
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-13 19:19:45 +0900

    without using Sealed keyword

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From Teacher");
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
+        }
+    }
+}
+/*The way we implement the singleton design pattern 
+ * in the above example is not to thread-safe.
+ * It means in a multithreaded environment,
+ * it will able to create multiple instances of the singleton
+ * class. How to make the singleton class thread-safe we will discuss
+ * in the Thread-Safe Singleton Design Pattern article.
+ * */
---
+++ DesignPattern/Singleton/Singleton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern
+{
+    /*
+     * Let us understand why Singleton class Sealed in C# with examples.
+     * First, create the Singleton class without using the sealed keyword. We also create another class with the name DerivedSingleton 
+     * and then Inherits it from the singleton class as shown below.     * 
+     * */
+    public class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+        public static Singleton GetInstance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter Value " + counter.ToString());
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+
+    //‘Singleton.Singleton()’ is inaccessible due to its protection level
+    public class DerivedSingleto : Singleton
+    {
+    }
+}
+/*
+ * The above error is because of the private constructor 
+ * that we have in the Singleton class. Now you might be thinking 
+ * that as we have a private constructor within the class so
+ * it is not possible to derive this class, then why do we need to apply the sealed keyword to the Singleton class.
+ * */

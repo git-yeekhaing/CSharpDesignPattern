2be063ac8910ac14d90260f38e9fe23dc1c57dd2
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-14 00:58:43 +0900

    static class in c#

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DesignPattern.Static;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,24 +11,30 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            //to invoke multiple methods parallelly
-            Parallel.Invoke(
-                () => PrintTeacherDetails(),
-                () => PrintStudentdetails());
+            Console.WriteLine("Please select the convertor direction");
+            Console.WriteLine("1. From Celsius to Fahrenheit.");
+            Console.WriteLine("2. From Fahrenheit to Celsius.");
+            Console.Write(":");
+
+            string selection = Console.ReadLine();
+            switch (selection)
+            {
+                case "1":
+                    Console.Write("Please enter the Celsius temperature: ");
+                    double F = TemperatureConverter.CelsiusToFahrenheit(Console.ReadLine());
+                    Console.WriteLine("Temperature in Fahrenheit: {0:F2}", F);
+                    break;
+                case "2":
+                    Console.Write("Please enter the Fahrenheit temperature: ");
+                    double C = TemperatureConverter.FahrenheitToCelsius(Console.ReadLine());
+                    Console.WriteLine("Temperature in Celsius: {0:F2}", C);
+                    break;
+                default:
+                    Console.WriteLine("Please select a convertor.");
+                    break;
+            }
 
             Console.ReadLine();
-        }
-
-        private static void PrintTeacherDetails()
-        {
-            Singleton fromTeacher = Singleton.GetInstance;
-            fromTeacher.PrintDetails("From Teacher");
-        }
-
-        private static void PrintStudentdetails()
-        {
-            Singleton fromStudent = Singleton.GetInstance;
-            fromStudent.PrintDetails("From Student");
         }
     }
 }

1d32743b61d4ef22b7bf6e2836a71b823b0a5bc8
myanmmar-polestar <mm.coursefinder@gmail.com>
2020-06-13 21:05:33 +0900

    Problem of Singleton in Parallel invoke

--- DesignPattern/Program.cs
+++ DesignPattern/Program.cs
@@ -10,23 +10,29 @@ namespace DesignPattern
     {
         static void Main(string[] args)
         {
-            Singleton fromTeacher = Singleton.GetInstance;
-            fromTeacher.PrintDetails("From Teacher");
-            Singleton fromStudent = Singleton.GetInstance;
-            fromStudent.PrintDetails("From Student");
-
-            Singleton.DerivedSingleton derivedObj = new Singleton.DerivedSingleton();
-            derivedObj.PrintDetails("From Derived");
+            //to invoke multiple methods parallelly
+            Parallel.Invoke(
+                () => PrintTeacherDetails(),
+                () => PrintStudentdetails());
 
             Console.ReadLine();
         }
+
+        private static void PrintTeacherDetails()
+        {
+            Singleton fromTeacher = Singleton.GetInstance;
+            fromTeacher.PrintDetails("From Teacher");
+        }
+
+        private static void PrintStudentdetails()
+        {
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+        }
+
+
     }
 }
 /*
-The above output clearly shows that the counter value
-has incremented to 2 which proves that the private constructor 
-executed twice and hence it creates multiple instances of the 
-singleton class. So, by removing the sealed keyword we can inherit 
-the singleton class and also possible to create multiple objects 
-of the singleton class. This violates singleton design principles.
- */
+ * 
+*/
--- DesignPattern/Singleton/Singleton.cs
+++ DesignPattern/Singleton/Singleton.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 
 namespace DesignPattern
 {
-    /*
-     * Let us understand why Singleton class Sealed in C# with examples.
-     * First, create the Singleton class without using the sealed keyword. We also create another class with the name DerivedSingleton 
-     * and then Inherits it from the singleton class as shown below.     * 
-     * */
     public sealed class Singleton
     {
         private static int counter = 0;
@@ -37,12 +32,5 @@ namespace DesignPattern
         {
             Console.WriteLine(message);
         }
-
-        // nested class
-        public class DerivedSingleton : Singleton
-        {
-        }
     }    
 }
-//we cannot derive a sealed class.
-
